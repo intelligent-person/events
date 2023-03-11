56439107135030543a68bbfcfa8d7f4c05ff6567
intelligent-person <tanyakupach@gmail.com>
2023-03-11 19:23:22 +0100

    initial commit

---
+++ apps/web-app/components/test/test.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+/* eslint-disable-next-line */
+export interface TestProps {}
+
+const StyledTest = styled.div`
+  color: pink;
+`;
+
+export function Test(props: TestProps) {
+  return (
+    <StyledTest>
+      <h1>Welcome to Test!</h1>
+    </StyledTest>
+  );
+}
+
+export default Test;
